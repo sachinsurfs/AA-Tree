f4707fbee4cf29befd8769c457d29feb3c46477c
Gautham V Kidiyoor <vk_gautham@ymail.com>
2015-05-16 23:25:35 -0700

    Node insertion

--- aa.cpp
+++ aa.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "aa.h"
 
-Node::Node(int value):_value(value),left(0),right(0),rb(BLACK),_level(1){};
+Node::Node(int value):value(value),left(0),right(0),level(1){};
+Node::Node():value(0),left(0),right(0),level(1){};
 
-Tree::Tree():root(0){};
+//Tree::Tree():root(0){};
 
 Tree::Tree(int value):root(new Node(value)){};
 
@@ -46,7 +47,146 @@ void Tree::inOrder(struct Node *head) const
     if(head != NULL)
     {
         inOrder(head->left);
-        std::cout << head->_value << "\n";
+        std::cout << head->value << "\n";
         inOrder(head->right);
     }
 }
+
+/*
+ * Insert String into the Tree
+ */
+int Tree::lookup(int &value)
+{
+    Node *temp = new Node();
+    temp->value = value;
+    temp->level = 1;
+    temp->count = 0;
+    temp->left = NULL;
+    temp->right = NULL;
+    temp->parent = NULL;
+    temp = insert(root, temp);
+    return temp->count;
+}
+ 
+/*
+ * Skew Tree
+ */
+ 
+void Tree::skew(Node *temp)
+{
+    Node *ptr = temp->left;
+    if (temp->parent->left == temp)
+        temp->parent->left = ptr;
+    else
+        temp->parent->right = ptr;
+    ptr->parent = temp->parent;
+    temp->parent = ptr;
+    temp->left = ptr->right;
+    if (temp->left != NULL)
+      temp->left->parent = temp;
+    ptr->right = temp;
+    temp->level = (temp->left ? temp->left->level + 1 : 1);
+}
+ 
+/*
+ * Splitting of AA Tree
+ */
+bool Tree::split(Node *temp)
+{
+    Node* ptr = temp->right;
+    if (ptr && ptr->right && (ptr->right->level == temp->level))
+    {
+        if (temp->parent->left == temp)
+            temp->parent->left = ptr;
+        else
+            temp->parent->right = ptr;
+        ptr->parent = temp->parent;
+        temp->parent = ptr;
+        temp->right = ptr->left;
+        if (temp->right != NULL)
+            temp->right->parent = temp;
+        ptr->left = temp;
+        ptr->level = temp->level + 1;
+        return true;
+    }
+    return false;
+}
+ 
+/*
+ * Rebalancing of AA Tree
+ */
+void Tree::rebal(Node* temp)
+{
+    temp->left = NULL;
+    temp->right = NULL;
+    temp->level = 1;
+    for (temp = temp->parent; temp != root; temp = temp->parent)
+    {
+        if (temp->level != (temp->left ? temp->left->level + 1 : 1 ))
+        {
+            skew(temp);
+            if (temp->right == NULL)
+                temp = temp->parent;
+            else if (temp->level != temp->right->level)
+                temp = temp->parent;
+        }
+        if (temp->parent != root)
+        {
+            if (split(temp->parent) == false)
+                break;
+        }
+    }
+}
+ 
+/*
+ * Insert Function to insert string into the tree
+ */
+Node* Tree::insert(Node* temp, Node* ins)
+{
+    if (root == NULL)
+    {
+        ins->count = 1;
+        ins->parent = NULL;
+        ins->left = NULL;
+        ins->right = NULL;
+        root = ins;
+        return root;
+    }
+    if (ins->value < temp->value)
+    {
+        if (temp->left)
+            return insert(temp->left, ins);
+        temp->left = ins;
+        ins->parent = temp;
+        ins->count = 1;
+        rebal(ins);
+        return ins;
+    }
+    if (ins->value > temp->value)
+    {
+        if (temp->right)
+            return insert(temp->right, ins);
+        temp->right = ins;
+        ins->parent = temp;
+        ins->count = 1;
+        rebal(ins);
+        return ins;
+    }
+    temp->count++;
+    delete ins;
+    return temp;
+}
+ 
+/*
+ * Display Tree Elements
+ */
+void Tree::print(Node* temp)
+{
+    if (!temp)
+        return;
+    print(temp->left);
+    std::cout <<"Value: "<<temp->value << "  Count:" << temp->count;
+    std::cout<<"  Level: "<<temp->level<< std::endl;
+    print(temp->right);
+}
+
--- client.cpp
+++ client.cpp
@@ -1,13 +1,48 @@
 #include <iostream>
 #include "aa.h"
 
+using namespace std;
 int main()
 {
-    Tree t2();
-    Tree t(5);    
+
+    //Tree t2();
+    //Tree t(5);    
     //t.insert(3);
     //t.insert(7);
     //t.insert(1);
     //t.display();
+    std::cout << "***********" << "\n" ;
+    int i = 3;
+    Tree newTree;
+    
+    std::cout << "inserting 3" << "\n" ;
+    newTree.lookup(i);
+    
+    std::cout << "inserting 4" << "\n" ;
+    i = 4;
+    newTree.lookup(i);
+
+    std::cout << "inserting 2" << "\n" ;
+    i = 2;
+    newTree.lookup(i);
+
+
+    std::cout << "inserting 7" << "\n" ;
+    i = 7;
+    newTree.lookup(i);
+
+    std::cout << "inserting 10" << "\n" ;
+    i = 10;
+    newTree.lookup(i);
+
+    std::cout << "inserting 8" << "\n" ;
+    i = 8;
+    newTree.lookup(i);
+    
+ 	
+	std::cout << "printing ..." << "\n" ;
+    newTree.print(newTree.root);
+    
+    
     return 0;
 }
